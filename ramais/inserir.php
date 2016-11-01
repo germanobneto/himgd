2f4feba9a3aa9b15ee0611ca8981b5ec03d9e705
Germano Neto <germano.neto@dafiti.com.br>
2016-11-01 20:14:07 -0200

    fix on file for project ramais

--- ramais/inserir.php
+++ ramais/inserir.php
@@ -7,63 +7,27 @@ $departamento = $_GET["txtDepartamento"];
 $cargo = $_GET["txtCargo"];
 $sexo = $_GET["rdSexo"];
 
-$conexao = mysql_connect("localhost","root","abc.123");
+$conexao = mysql_connect("localhost","root","root");
 $banco = mysql_select_db("ramal");
 
 
-	$sql_insert_tb_funcionario = "insert into tb_funcionario  (fun_nome, fun_cracha, fun_sexo)
+$sql_insert_tb_funcionario = "insert into tb_funcionario  (fun_nome, fun_cracha, fun_sexo)
 
-														values ('$nome',$cracha,'$sexo')";
-
-
-	$sql_insert_tb_departamento = "insert into tb_departamento  (nome_dept)
-
-														values ('$departamento')";
+														values ('$nome',$cracha, '$sexo')";
 
 
-			$sql_insert_tb_cargo = "insert into tb_cargo  (car_nome)
 
 
-									values ('$cargo')";
 
 
 $resultado = mysql_query($sql_insert_tb_funcionario,$conexao);
 if ($resultado){
-	$numeroRegistros = mysql_affected_rows($conexao);
+	$numeroRegistros = mysql_affected_rows($resultado);
 	echo "Comando executado com sucesso. ";
 	echo "Foram afetados $numeroRegistros registros.";
 }else{
   echo "Falha ao executar comando.";
 }
-
-$resultado2 = mysql_query($sql_insert_tb_departamento,$conexao);
-if ($resultado){
-	$numeroRegistros = mysql_affected_rows($conexao);
-	echo "Comando executado com sucesso. ";
-	echo "Foram afetados $numeroRegistros registros.";
-}else{
-  echo "Falha ao executar comando.";
-}
-
-
-
-
-$resultado3 = mysql_query($sql_insert_tb_cargo,$conexao);
-if ($resultado){
-	$numeroRegistros = mysql_affected_rows($conexao);
-	echo "Comando executado com sucesso. ";
-	echo "Foram afetados $numeroRegistros registros.";
-}else{
-  echo "Falha ao executar comando.";
-}
-
-
-
-
-
-
-
-
 
 mysql_close($conexao);
 ?>
